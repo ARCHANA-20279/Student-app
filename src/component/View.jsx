ce2676b3e52ed022ddb640496192401022dc3511
ARCHANA-20279 <archana552m@gmail.com>
2026-07-10 11:25:43 +0530

    dynamic changes added

--- src/component/View.jsx
+++ src/component/View.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navigation from './Navigation'
 
 function View() {
+
+const [data,changeData]=useState(
+  [{"si no":1,"name":"archana","class":"mca","admission number":123},
+  {"si no":2,"name":"archana","class":"mca","admission number":789},
+    {"si no":3,"name":"archana","class":"mca","admission number":111},
+    {"si no":4,"name":"archana","class":"mca","admission number":456},
+    {"si no":5,"name":"archana","class":"mca","admission number":234},
+  ]
+)
+
   return (
     <div>
           <Navigation />
           <div classNameName="container" style={{padding:10,margin:50}}>
 <h1 style={{ color: "blue", marginBottom: 50,  }}>
-  DETAILS OF STUDENTS
+  DETAILS OF STUDENTS 👨‍🎓
 </h1>
 
         <div className="container">
@@ -15,52 +25,36 @@ function View() {
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-col-xxl-12">
                     <div className="container">
                         <div className="row">
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                
-                                <table class="table">
+
+                          <table class="table">
   <thead>
     <tr>
       <th scope="col">SI NO</th>
+      <th scope="col">NUMBER</th>
       <th scope="col">NAME</th>
-      <th scope="col">ADMISSION NO.</th>
-           <th scope="col">CLASS</th>
-
+      <th scope="col">CLASS</th>
     </tr>
   </thead>
   <tbody>
+ 
+                            {data.map(
+                              (value,index)=>{
+                                return(
+                                
+                               
     <tr>
-      <th scope="row">1</th>
-      <td>Archana</td>
-      <td>123</td>
-      <td>MCA</td>
+      <th scope="col">{value['si no']}</th>
+      <th scope="col">{value['admission number']}</th>
+      <th scope="col">{value.name}</th>
+           <th scope="col">{value.class}</th>
+
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>345</td>
-      <td>MBA</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>789</td>
-      <td>BTECH</td>
-    </tr>
-     <tr>
-      <th scope="row">3</th>
-      <td>john</td>
-      <td>785</td>
-      <td>BTECH</td>
-    </tr>
-     <tr>
-      <th scope="row">3</th>
-      <td>helen</td>
-      <td>745</td>
-      <td>MCA</td>
-    </tr>
-  </tbody>
-</table>
-                            </div>
+  
+                            )
+                              }
+                            )}
+                            </tbody>
+                            </table>
                             
                         </div>
                     </div>
